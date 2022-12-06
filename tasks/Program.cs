6d94da6491d8783b7bce3a41854c0fa8f8b164ef
Oksana <avalonv264@gmail.com>
2022-12-06 22:20:14 +0300

    tasks

---
+++ tasks/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+ 
+Console.Write($" Ведите трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);     
+
+

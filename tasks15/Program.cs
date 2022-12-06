df789ecf2e214d7aa229d1bfd3507736ad0c2ad0
Oksana <avalonv264@gmail.com>
2022-12-06 23:04:16 +0300

    tasks15

---
+++ tasks15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Ведите цифру,обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingTheDayOfTheWeek (int dayNumber) {
+    if (dayNumber == 6 || dayNumber == 7){
+        Console.WriteLine("( Этот день выходной) -> да");
+    }
+else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Это не день недели");
+}
+else Console.WriteLine("(Этот день не выходной) -> нет");
+}
+CheckingTheDayOfTheWeek(dayNumber);

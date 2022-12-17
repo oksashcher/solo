7fcabbbda562fbf7e63068a5e97112954ec5bd45
Oksana <avalonv264@gmail.com>
2022-12-17 12:20:31 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("ведите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int number){
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+    for (int i = 0; i < counter; i++){
+        advance = numberN - number % 10;
+        result = result + (numberN - advance);
+        number = number / 10;
+    }
+    return result;
+}
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("сумма цифр в числе: " + sumNumber);
+

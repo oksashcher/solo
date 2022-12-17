4f44b977f1304a4c032f86df114531d59c89ca3e
Oksana <avalonv264@gmail.com>
2022-12-17 12:55:34 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write("ведите ряд чисел, разделенных запятой: ");
+string? seriesOfNumbers = Console.ReadLine();
+seriesOfNumbers = seriesOfNumbers + ",";
+string RemovingSpaces (string srries){
+    string seriesNew = "";
+    for (int i = 0; i < series.Length; i++)
+    {
+        if (series[i] != '')
+        {
+            seriesNew += series[i];
+        }
+    }
+    return seriesNew;
+}
+void PrintArry(int[] coll){
+    int count = coll.Length;
+    int index = 0;
+    Console.Write("[");
+    while(index <count){
+        Console.Write(coll[index]);
+        index++;
+        if (index < count){
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+string seriesNew = RemovingSpaces(seriesOfNumbers);
+int[] arrayOfNumbers = ArrayOfNumbers(seriesNew);
+PrintArry(arrayOfNumbers);
+

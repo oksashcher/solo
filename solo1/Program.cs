6c2d83c1e00594bd5b0fa911f9257f647a2b92d9
Oksana <avalonv264@gmail.com>
2022-12-23 23:08:13 +0300

    solo1

---
+++ solo1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Ведите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] mass = new int [n]; 
+for (int i = 0; i < n; i++)
+{ Console.Write($"Ведите {i}й элемент: "); mass[i] = Convert.ToInt32(Console.ReadLine());}
+int min = 0;
+int max = 0;
+foreach (int i in mass)
+{
+    if (min > i) min = i;
+    if (max < i) max = i;
+}
+Console.WriteLine($"\n Максимальный элемент: {max}\n  Минимальный элемент: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {max- min}" );

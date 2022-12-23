8d8ad8074f41be301bef515d6efb0c2aca695782
Oksana <avalonv264@gmail.com>
2022-12-23 23:26:30 +0300

    solo2

---
+++ solo2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Ведите размер массива: ");
+int size = 
+Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers (numbers);
+Console.WriteLine(" Вот наш массив: ");
+PrintArray (numbers);
+int count = 0;
+for (int z = 0; z< numbers.Length; z++);
+if (numbers [z] % 2 == 0);

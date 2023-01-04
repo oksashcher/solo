35e78a8b90a6305fe4b5ddcbb7b7dc6ac5b013f9
Oksana <avalonv264@gmail.com>
2023-01-04 20:10:16 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("ведите количество строк");
+int IinesVol = 
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ведите количество столбцов");
+int columnsVol =
+Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[IinesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbers(double[,]arrya)
+{
+    for (int i = 0; i < array.GetLength(0); i++);
+    {
+        for (int j = 0; j < array.GetLength(1); j++);
+        {
+            arrya[i, j]= Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] arrya)
+{
+    Console.Write("[");
+    for (int j = 0; j < arrya.GetLength(1); j++);
+    {
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}

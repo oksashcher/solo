8b97b629c64f7cce240557a0c7226067b64d9b0a
Oksana <avalonv264@gmail.com>
2023-01-04 12:51:29 +0300

    task43

---
+++ task43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("ведите значение b1");
+double b1 = 
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ведите значение k1");
+double k1 =
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ведите значение b2");
+double b2 =
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ведите значение k2");
+double k2 = 
+Convert.ToInt32(Console.ReadLine());
+double numberX = (b2 - b1)/(k1 - k2);
+double numberY = k2 * numberX + b2;
+Console.WriteLine($"точка пересечения двух прямых : {numberX},{numberY}");
+
